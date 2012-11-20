62a07006b655f8ac37246aeb01542806b0923250
Jim Fulton <jim@zope.com>
2012-11-20 21:47:22 +0000

    version

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@
 # FOR A PARTICULAR PURPOSE.
 #
 ##############################################################################
-name, version = "zc.lockfile", 1.0.1
+name, version = "zc.lockfile", '1.0.1'
 
 import os
 from setuptools import setup, find_packages
