0b495f8b6f9eb613ce2bb664a8d4e23b6f280afc
Jason R. Coombs <jaraco@jaraco.com>
2018-09-11 14:37:40 -0400

    Add test for SimpleLockFile, ensuring basic lock expectation.

--- src/zc/lockfile/tests.py
+++ src/zc/lockfile/tests.py
@@ -179,6 +179,18 @@ class LockFileLogEntryTestCase(unittest.TestCase):
         lock.close()
         p.join()
 
+    def test_simple_lock(self):
+        assert isinstance(zc.lockfile.SimpleLockFile, type)
+        lock = zc.lockfile.SimpleLockFile('s')
+        try:
+            zc.lockfile.SimpleLockFile('s')
+        except zc.lockfile.LockError:
+            pass
+        else:
+            raise Exception("LockError not raised")
+        lock.close()
+        zc.lockfile.SimpleLockFile('s').close()
+
 
 def test_suite():
     suite = unittest.TestSuite()
